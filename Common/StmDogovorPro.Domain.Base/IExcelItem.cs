c11962792b1dc2268f93ddd544d19d8d8c692844
Aleksey N <nester.a.l@yandex.ru>
2022-06-17 21:28:29 +0300

    добавление сущностей данных из документов

--- Common/StmDogovorPro.Domain.Base/IExcelItem.cs
+++ Common/StmDogovorPro.Domain.Base/IExcelItem.cs
@@ -1,7 +1,11 @@
-﻿namespace StmDogovorPro.Domain.Base
+﻿using StmDogovorPro.Domain.Base.ExcellBase;
+
+namespace StmDogovorPro.Domain.Base
 {
     public interface IExcelItem : INamedItem
     {
-
+        IAnnualVolume AnnualVolume { get; set; }
+        IFirstBatch FirstBatch { get; set; }
+        IRCElement RC { get; set; }
     }
 }
