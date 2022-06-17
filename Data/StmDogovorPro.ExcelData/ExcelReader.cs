96f031e0024857a696d9f755bdde5d7362319855
Aleksey N <nester.a.l@yandex.ru>
2022-06-17 23:08:56 +0300

    работа с классом читателя

--- Data/StmDogovorPro.ExcelData/ExcelReader.cs
+++ Data/StmDogovorPro.ExcelData/ExcelReader.cs
@@ -1,13 +1,41 @@
-﻿using StmDogovorPro.Services.Base;
-using Excel = Microsoft.Office.Interop.Excel;
+﻿using System.Text;
+using ExcelDataReader;
+using StmDogovorPro.Services.Base;
 
 namespace StmDogovorPro.ExcelData
 {
     public class ExcelReader : IReader
     {
+        static CodePagesEncodingProvider code;
         public ICollection<string> Read(string path)
         {
-            throw new NotImplementedException();
+
+            return default;
+        }
+
+        public void TestRead()
+        {
+            var path = AppDomain.CurrentDomain.BaseDirectory;
+            var excelPath = "excelTest.xlsx";
+
+            var testPath = Path.Combine(path, excelPath);
+
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            using (var file = File.Open(testPath, FileMode.Open))
+            {
+                using (var app = ExcelReaderFactory.CreateReader(file))
+                {
+                    var result = app.AsDataSet();
+                    var reader = result.CreateDataReader();
+                    while (reader.Read())
+                    {
+                        //нужно как-то прочесть фаил
+                        var one = reader.GetString(0);
+                    }
+
+                }
+            }
+            
         }
     }
 }
