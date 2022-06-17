a0f936f1d8eb3ab0fa4ecb8a07d673bb2f1bf5d1
Aleksey N <nester.a.l@yandex.ru>
2022-06-17 21:44:40 +0300

    добавление новых свойств

--- Common/StmDogovorPro.Domain.Base/IExcelItem.cs
+++ Common/StmDogovorPro.Domain.Base/IExcelItem.cs
@@ -4,6 +4,8 @@ namespace StmDogovorPro.Domain.Base
 {
     public interface IExcelItem : INamedItem
     {
+        decimal CostWithTaxes { get; set; }
+        decimal SaleCost { get; set;}
         IAnnualVolume AnnualVolume { get; set; }
         IFirstBatch FirstBatch { get; set; }
         IRCElement RC { get; set; }
