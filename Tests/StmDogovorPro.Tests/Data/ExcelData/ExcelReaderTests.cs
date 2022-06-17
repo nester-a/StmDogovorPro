890ae95a7549391c0af1bed295d398292990c70d
Aleksey N <nester.a.l@yandex.ru>
2022-06-17 23:08:47 +0300

    начало тестирования класса читателя

---
+++ Tests/StmDogovorPro.Tests/Data/ExcelData/ExcelReaderTests.cs
@@ -0,0 +1,32 @@
+﻿using StmDogovorPro.ExcelData;
+using System;
+using System.IO;
+using Xunit;
+
+namespace StmDogovorPro.Tests.Data.ExcelData
+{
+    public class ExcelReaderTests
+    {
+        ExcelReader excel = new ExcelReader();
+
+        [Fact]
+        public void ReadTest()
+        {
+            var path = AppDomain.CurrentDomain.BaseDirectory;
+            var excelPath = "excelTest.xlsx";
+
+            var testPath = Path.Combine(path, excelPath);
+            bool catched = false;
+            try
+            {
+                excel.TestRead();
+            }
+            catch (NotImplementedException e)
+            {
+                catched = true;
+                Assert.True(e is NotImplementedException);
+            }
+            Assert.True(catched);
+        }
+    }
+}
