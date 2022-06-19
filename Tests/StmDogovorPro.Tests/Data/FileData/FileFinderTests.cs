86bdadd06914e6bc1e37242c110619aeb39d7fc0
Aleksey N <nester.a.l@yandex.ru>
2022-06-19 11:22:58 +0300

    FileFinder и его тестирование

---
+++ Tests/StmDogovorPro.Tests/Data/FileData/FileFinderTests.cs
@@ -0,0 +1,42 @@
+﻿using StmDogovorPro.FileData;
+using StmDogovorPro.Services.Base;
+using System;
+using System.IO;
+using Xunit;
+namespace StmDogovorPro.Tests.Data.FileData
+{
+    public class FileFinderTests
+    {
+        [Fact]
+        public void FindTest()
+        {
+            IFileFinder fileFinder = new FileFinder();
+
+            var path = AppDomain.CurrentDomain.BaseDirectory;
+            var excelPath = "excelTest.xlsx";
+            var testPath = Path.Combine(path, excelPath);
+
+            fileFinder.Path = testPath;
+
+            fileFinder.Find();
+
+            Assert.True(fileFinder.Exist);
+        }
+
+        [Fact]
+        public void FindTest_Exist_is_False()
+        {
+            IFileFinder fileFinder = new FileFinder();
+
+            var path = AppDomain.CurrentDomain.BaseDirectory;
+            var excelPath = "hello.xlsx";
+            var testPath = Path.Combine(path, excelPath);
+
+            fileFinder.Path = testPath;
+
+            fileFinder.Find();
+
+            Assert.False(fileFinder.Exist);
+        }
+    }
+}
